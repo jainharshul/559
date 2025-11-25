3a34efe83b46317469e3e57cf8f1b796cb8fddfd
Andrew Null <andrew@nulls.us>
2025-11-25 12:01:43 -0700

    divy.py revised

--- src/divy.py
+++ src/divy.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+
 if __name__ == "__main__":
     infn = sys.argv[1]
-    outdir = sys.argv[2]
+    outdir = ""
+    if len(sys.argv) == 3:
+        outdir = sys.argv[2]
     inf = open(infn, "r")
     header = inf.readline()
 
     names = ["andrew", "daniel", "hershey", "curry"]
-    splitxt = [[]] * len(names)
+    splitxt = [[] for n in names]
+	# [[]] * len(names) results in copies of the reference at each index, which is to say doesn't work
+    assert len(splitxt) == len(names)
     dex = 0
     for line in inf:
         splitxt[dex].append(line)
@@ -16,7 +21,7 @@ if __name__ == "__main__":
 
 
 
-    for d in range(0,len(names):
+    for d in range(0,len(names)):
         outf = open(outdir + names[d] + "_" + infn, "w+")
         outf.write(header)
         for txt in splitxt[d]:
