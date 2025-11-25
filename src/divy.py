49c47105a9709dad6ec49788d1ef243be3edfc06
Andrew Null <andrew@nulls.us>
2025-11-25 09:26:55 -0700

    CSVs divyed with header

--- src/divy.py
+++ src/divy.py
@@ -3,6 +3,7 @@
 import sys
 if __name__ == "__main__":
     infn = sys.argv[1]
+    outdir = sys.argv[2]
     inf = open(infn, "r")
     header = inf.readline()
 
@@ -16,7 +17,8 @@ if __name__ == "__main__":
     names = ["andrew", "daniel", "hershey", "curry"]
 
     for d in range(0,4):
-        outf = open(names[d] + "_" + infn, "w+")
+        outf = open(outdir + names[d] + "_" + infn, "w+")
+        outf.write(header)
         for txt in splitxt[d]:
             outf.write(txt)
         outf.close()
