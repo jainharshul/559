069810c5afb65c61e1515adf92ea8529a2200b5f
Andrew Null <andrew@nulls.us>
2025-11-30 15:55:23 -0700

    divyed by HLA

--- src/divy.py
+++ src/divy.py
@@ -2,6 +2,14 @@
 
 import sys
 
+def verify(d, h, l):
+    if h not in d.keys():
+        print(h)
+        d[h] = []
+
+    d[h].append(l)
+    return d
+
 if __name__ == "__main__":
     infn = sys.argv[1]
     outdir = ""
@@ -10,20 +18,37 @@ if __name__ == "__main__":
     inf = open(infn, "r")
     header = inf.readline()
 
+    hdex = len(header)
+    print(header)
+    splead = header.split(",")
+    for head in range(0,len(splead)):
+        hdex = head if splead[head] == "HLA" else hdex
+    print(hdex)
+
     names = ["andrew", "daniel", "hershey", "curry"]
+    names = set()
     splitxt = [[] for n in names]
+    buckets = dict()
 	# [[]] * len(names) results in copies of the reference at each index, which is to say doesn't work
     assert len(splitxt) == len(names)
     dex = 0
     for line in inf:
-        splitxt[dex].append(line)
-        dex = (dex + 1) % len(splitxt)
+        hla = line.split(",")[hdex]
+        buckets = verify(buckets, hla, line)
+        #splitxt[dex].append(line)
+        #dex = (dex + 1) % len(splitxt)
+
+    names = list(buckets.keys())
+    print(names)
+    #names = [name.replace("/", "|") for name in names]
 
 
 
     for d in range(0,len(names)):
-        outf = open(outdir + names[d] + "_" + infn, "w+")
+        #break
+        nm = outdir + names[d].replace("/", "|") + "_" + infn
+        outf = open(nm, "w+")
         outf.write(header)
-        for txt in splitxt[d]:
+        for txt in buckets[names[d]]:
             outf.write(txt)
         outf.close()
