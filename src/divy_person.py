f63616f105911928845c7560673bbce6fc2f9be0
Andrew Null <andrew@nulls.us>
2025-12-04 22:33:59 -0700

    Re-divyed

--- src/divy_person.py
+++ src/divy_person.py
@@ -7,18 +7,18 @@ if __name__ == "__main__":
     inf = open(infn, "r")
     header = inf.readline()
 
-    names = ["andrew", "daniel", "hershey", "curry"]
-    splitxt = [[]] * len(names)
+    names = ["daniel", "hershey", "curry"]
+    splitxt = [[] for x in range(0,len(names))]
     dex = 0
     for line in inf:
+        #print(line)
         splitxt[dex].append(line)
         dex = (dex + 1) % len(splitxt)
 
-
-
-    for d in range(0,len(names):
+    for d in range(0,len(names)):
         outf = open(outdir + names[d] + "_" + infn, "w+")
         outf.write(header)
         for txt in splitxt[d]:
+            #print(f"Writing: {txt}")
             outf.write(txt)
         outf.close()
